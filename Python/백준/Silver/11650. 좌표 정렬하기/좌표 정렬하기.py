eb782db2720655e6351e08a2af47a0fdabce98b9
hhaminlee <142904579+hhaminlee@users.noreply.github.com>
2025-07-21 21:15:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 296 ms, Memory: 55000 KB -BaekjoonHub

---
+++ Python/백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    # 튜플 형식으로 저장해서 x,y 쌍으로 저장하기
+    a.append((x, y))
+# x값을 기준으로 하되, 같으면 y값을 기준으로 정렬하기
+sorted_items = sorted(a, key = lambda x: (x[0], x[1]))
+for i in range(n):
+    print(sorted_items[i][0], sorted_items[i][1])
